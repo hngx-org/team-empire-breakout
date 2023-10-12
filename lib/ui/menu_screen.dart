7f4ada595a9197879a6fd4025c4a1b7c89c6c8d6
Ayoade Adesope <ezesamuel@Klassiq-MacBook-Pro.local>
2023-10-12 14:26:20 +0100

    Instuction screen and modification of Menu screen

--- lib/ui/menu_screen.dart
+++ lib/ui/menu_screen.dart
@@ -1,66 +1,8 @@
-<<<<<<< HEAD
-// import 'package:emp_breakout/components/animated_button.dart';
-// import 'package:emp_breakout/ui/game_screen.dart';
-// import 'package:flutter/material.dart';
-//
-// class MenuContent extends StatefulWidget {
-//   @override
-//   _MenuContentState createState() => _MenuContentState();
-// }
-//
-// class _MenuContentState extends State<MenuContent> {
-//   double buttonSize = 100.0;
-//   bool isButtonBouncing = false;
-//
-//   void startGame() {
-//     setState(() {
-//       isButtonBouncing = true;
-//       buttonSize = 120.0;
-//     });
-//
-//     // Simulate a delay for the animation
-//     Future.delayed(Duration(milliseconds: 300), () {
-//       setState(() {
-//         isButtonBouncing = false;
-//         buttonSize = 100.0;
-//       });
-//
-//       // Navigate to the game screen or perform other actions
-//       // You can replace this with your game logic.
-//       // Example: Navigator.push(context, MaterialPageRoute(builder: (context) => GameScreen()));
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       mainAxisAlignment: MainAxisAlignment.center,
-//       crossAxisAlignment: CrossAxisAlignment.center,
-//       children: <Widget>[
-//         AnimatedContainer(
-//           duration: Duration(milliseconds: 300),
-//           width: buttonSize,
-//           height: buttonSize,
-//           child: ElevatedButton(
-//             onPressed: () {
-//               Navigator.pushReplacement(
-//               context,
-//               MaterialPageRoute(
-//               builder: (context) => MainGameScreen()),
-//     );},
-//             child: Text('Start Game'),
-//         ),
-//         ),
-//         AnimatedButton(),
-//
-//       ],
-//     );
-//   }
-// }
+
 import 'package:emp_breakout/ui/instructions_screen.dart';
-=======
+
 import 'package:emp_breakout/ui/levels_screen.dart';
->>>>>>> 30e58ad352b7615c7f1a72c59317cca9099a8d60
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -142,11 +84,11 @@ class _PageWithAnimatedListState extends State<MenuScreen> {
           onPressed: () {
             Navigator.push(
               context,
-<<<<<<< HEAD
-              MaterialPageRoute(builder: (context) => InstructionScreen())
-=======
-              MaterialPageRoute(builder: (context) => LevelScreen()),
->>>>>>> 30e58ad352b7615c7f1a72c59317cca9099a8d60
+
+
+
+              MaterialPageRoute(builder: (context) => InstructionScreen()),
+
             );
           },
           child: Text('Instructions',
@@ -204,13 +146,7 @@ class _PageWithAnimatedListState extends State<MenuScreen> {
         ),
         child: Column(
           children: [
-<<<<<<< HEAD
             SizedBox(height: 200,),
-=======
-            SizedBox(
-              height: 300,
-            ),
->>>>>>> 30e58ad352b7615c7f1a72c59317cca9099a8d60
             Expanded(
               child: Stack(
                 children:[
